2f66efc820feaa95a3c1ee3cbf54e15c799e5732
Piyushjoshi21 <128383986+Piyushjoshi21@users.noreply.github.com>
2023-03-20 18:37:49 +0530

    Add files via upload

---
+++ proj.js
@@ -0,0 +1,7 @@
+let A = document.querySelector('button')
+A.addEventListener('click', msg)
+ function msg(){
+    A=prompt("Enter your password again (*for security reasons.)")
+    B=prompt("Enter your name as you filled in the form")
+    alert("Hello" +" "+B+" "+" Your form is sucessfull submitted . You will get a call within 24 hours . I totally appriciate that you visit our website . Thanks for visiting . For more Query contact us on :- Shirlaconsult123@gmail.com ")
+ }
